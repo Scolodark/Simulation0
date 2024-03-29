0c3e6bb1bb3a5ca0836caabb266ee3060b42bf89
gf04\User <sbsgameas@gmail.com>
2024-03-29 21:49:46 +0900

    second

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    [Header("¿Ãµø")]
+    [SerializeField] float moveSpeed;
+
+    private void Awake()
+    {
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void playerMove()
+    {
+        
+    }
+}
